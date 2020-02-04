a6c9cc37a12aba684ed7e4368a9437f900d63cb0
Dero <106393@gmail.com>
2020-02-04 14:09:41 +0400

    Updates

---
+++ precache-manifest.83904215d234e934cd2e3f63d3b13281.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0db0fe742e50b3ec67b6c200aae4b03b",
+    "url": "/react-Todo-app_Redux/index.html"
+  },
+  {
+    "revision": "22f83de3e650418daa21",
+    "url": "/react-Todo-app_Redux/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "b71234655e1f17723e83",
+    "url": "/react-Todo-app_Redux/static/css/main.23ebafaf.chunk.css"
+  },
+  {
+    "revision": "22f83de3e650418daa21",
+    "url": "/react-Todo-app_Redux/static/js/2.8206a34b.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/react-Todo-app_Redux/static/js/2.8206a34b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b71234655e1f17723e83",
+    "url": "/react-Todo-app_Redux/static/js/main.aba7fa74.chunk.js"
+  },
+  {
+    "revision": "56106f61645d50d8124f",
+    "url": "/react-Todo-app_Redux/static/js/runtime-main.944c067e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-Todo-app/precache-manifest.18c53cf0672064dad47ce7e52599cfbd.js"
+  "/react-Todo-app_Redux/precache-manifest.83904215d234e934cd2e3f63d3b13281.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-Todo-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-Todo-app_Redux/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
